ee9ce63527dfa3b777f19a6bc0ba488cf94d6471
Erik Nord <tpi13eno@student.lu.se>
2020-10-15 11:18:43 +0200

    cleaning code

--- express/app.js
+++ express/app.js
@@ -1,23 +1,33 @@
 'use strict';
+
 /* Imports */
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
+
 /* Middleware */
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 /* Define end points */
+
 const endpoints = Array();
 endpoints.push(require('./routes/grocery'));
 endpoints.push(require('./routes/recipe'));
 endpoints.push(require('./routes/section'));
 endpoints.push(require('./routes/user'));
+
 /* Use the defined endpoints */
+
 endpoints.forEach(e => app.use(e));
+
 /* Root end point */
+
 app.get('/', (req, res) => {
   console.log('HTTP GET  /');
   res.status(200).json({
