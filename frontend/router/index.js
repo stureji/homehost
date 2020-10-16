dfb5aadfd3088d1f896cb4877e3c19202a600f78
Erik Nord <tpi13eno@student.lu.se>
2020-10-16 18:20:37 +0200

    working on 404 page

--- frontend/router/index.js
+++ frontend/router/index.js
@@ -1,24 +1,34 @@
-import { createWebHistory, createRouter } from "vue-router";
-import Home from "@/views/Home.vue";
-import User from "@/views/User.vue";
-import ShoppingList from "@/views/ShoppingList.vue";
+import { createWebHistory, createRouter } from 'vue-router';
+import Home from '@/views/Home.vue';
+import User from '@/views/User.vue';
+import NotFound from '@/views/NotFound.vue';
+import ShoppingList from '@/views/ShoppingList.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/user",
-    name: "User",
+    path: '/user',
+    name: 'User',
     component: User
   },
   {
-    path: "/shoplist",
-    name: "Shopping List",
+    path: '/shoplist',
+    name: 'Shopping List',
     component: ShoppingList
-  }
+  },
+  {
+    path: '/404',
+    name: 'Not Found',
+    component: NotFound
+  },
+  // {
+  //   path: '*',
+  //   redirect: '/404'
+  // }
 ];
 
 const router = createRouter({
