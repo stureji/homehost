2f514e493fe595745271b9a195e594889afdd1da
Erik Nord <tpi13eno@student.lu.se>
2020-10-20 14:47:53 +0200

    making sure the data is correct before sending... will alter

--- express/src/routes/user.ts
+++ express/src/routes/user.ts
@@ -1,13 +1,15 @@
 import express  from 'express';
 import { pool } from '../database';
 import User, { UserJSON } from '../classes/data/User';
+import Grocery from '../classes/data/Grocery';
+import Section from '../classes/data/Section';
 
 const app = module.exports = express();
 
 app.get('/api/user', async (req: any, res: any) => {
   console.log('HTTP GET   /api/user');
 
-  const data: UserJSON[] = await pool.connect().then((connection) => {
+  const data: User[] = await pool.connect().then((connection) => {
     if(connection) {
       return pool.query('SELECT * FROM users');
     } else {
@@ -15,21 +17,27 @@ app.get('/api/user', async (req: any, res: any) => {
     }
   }).then((dbresult) => {
     return dbresult.rows.map( r => {
-      return new User(r.user_id, r.username).toJson();
+      return new User(r.user_id, r.username, r.shoplist);
     });
   }).catch((error) => {
     console.log(error);
-    return [ new User(0, 'error').toJson() ];
+    return [ new User(0, 'error') ];
   });
+
+  data[0].shoppinglist.add(new Grocery(1337, 'l33t root', new Section(1337, 'l33t place')));
 
   res.status(200).json({
     status: 200,
     message: "OK",
-    data
+    data: data.map(x => x.toJson())
   });
 });
 
 app.post('/api/user/login', (req: any, res: any) => {
+  const sverker = new User(0, 'Sverker Petersson');
+  const lina = new User(1, 'Bosse Bosinna');
+  const users = new Array<User>(sverker, lina);
+
   console.log('HTTP POST  /api/user/login');
   let status = 401;
   let message = "UNAUTHORIZED";
