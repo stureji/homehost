31b73dd1a54601d00f8a3b66829e4149bde94163
Erik Nord <tpi13eno@student.lu.se>
2020-10-21 12:46:14 +0200

    working ServerResponse before implement to system

--- express/src/classes/ServerResponse.ts
+++ express/src/classes/ServerResponse.ts
@@ -19,10 +19,10 @@ export default class ServerResponse {
     [500, 'INTERNAL_SERVER_ERROR']
   ]);
 
-  #json: ServerResponseJSON;
+  _json: ServerResponseJSON;
 
   constructor() {
-    this.#json = {
+    this._json = {
       status: 400,
       message: 'BAD_REQUEST'
     };
@@ -30,14 +30,16 @@ export default class ServerResponse {
 
   set status(code: number) {
     if(ServerResponse.ALLOWED_STATUS.has(code)) {
-      this.#json.status = code;
-      this.#json.message = ServerResponse.ALLOWED_STATUS.get(code);
+      this._json.status = code;
+      this._json.message = ServerResponse.ALLOWED_STATUS.get(code);
+    } else {
+      throw new Error('HTTP Status code [' + code + '] not supported!!!');
     }
   }
 
   set data(value: DataScheme<SchemeJSON>[] | null | undefined) {
     if(value != null && value != undefined) {
-      this.#json.data = value.map(d => d.toJson());
+      this._json.data = value.map(d => d.toJson());
       this.status = 200;
     } else {
       this.status = 204;
@@ -45,14 +47,52 @@ export default class ServerResponse {
   }
 
   set error(error: Error) {
-    this.#json.error = error.message;
+    this._json.error = error.message;
   }
 
   get json(): ServerResponseJSON {
-    if(this.#json.status < 500) {
-      return (({status, message, data}) => ({status, message, data}))(this.#json);
+    // There should be guard syntax for js!!!
+    const status: number = this._json.status;
+    if(status >= 100 && status < 700) {
+      if(status >= 100 && status < 200) {
+        return this.onInfo();
+      } else if(status >= 200 && status < 300) {
+        return this.onSucces();
+      } else if(status >= 300 && status < 400) {
+        return this.onRedirect();
+      } else if(status >= 400 && status < 500) {
+        return this.onClientError();
+      } else if(status >= 500 && status < 600) {
+        return this.onServerError();
+      }else {
+        return this.onIllegalRange();
+      }
     } else {
-      return (({status, message, error}) => ({status, message, error}))(this.#json);
+      return this.onIllegalRange();
     }
+  }
+
+  public onInfo(): ServerResponseJSON {
+    return (({status, message}) => ({status, message}))(this._json);
+  }
+
+  public onSucces(): ServerResponseJSON {
+    return (({status, message, data}) => ({status, message, data}))(this._json);
+  }
+
+  public onRedirect(): ServerResponseJSON {
+    return (({status, message}) => ({status, message}))(this._json);
+  }
+
+  public onClientError(): ServerResponseJSON {
+    return (({status, message, error}) => ({status, message, error}))(this._json);
+  }
+
+  public onServerError(): ServerResponseJSON {
+    return (({status, message}) => ({status, message}))(this._json);
+  }
+
+  public onIllegalRange(): ServerResponseJSON {
+    return {status: 500, message: "INTERNAL_SERVER_ERROR"};
   }
 }
--- express/test/classes/ServerResponseTest.ts
+++ express/test/classes/ServerResponseTest.ts
@@ -32,53 +32,78 @@ class TestData implements DataScheme<TestJSON> {
 }
 
 test('ServerResponse: constructed bar request default', t => {
-  const res = new ServerResponse();
-  t.is(res.json.status, 400, 'expecting bad request response code');
-  t.is(res.json.message, 'BAD_REQUEST', 'expecting BAD_REQUEST');
-  t.is(res.json.data, undefined, 'expect undefined');
-  t.is(res.json.error, undefined, 'expect undefined');
+  const response = new ServerResponse();
+  const res = response.json;
+  t.is(res.status, 400, 'expecting bad request response code');
+  t.is(res.message, 'BAD_REQUEST', 'expecting BAD_REQUEST');
+  t.is(res.data, undefined, 'expect undefined');
+  t.is(res.error, undefined, 'expect undefined');
+  t.is(JSON.stringify(res), '{"status":400,"message":"BAD_REQUEST"}', 'stringify equal to expected');
 });
 
 test('ServerResponse: adding [] data to request', t => {
-  const res = new ServerResponse();
+  const response = new ServerResponse();
   const td1: DataScheme<SchemeJSON>[] = [];
-  res.data = td1;
-  t.is(res.json.status, 200, 'expecting OK response code');
-  t.is(res.json.message, 'OK', 'expecting OK');
-  t.deepEqual(res.json.data, [], 'expect deep equality to passed value');
-  t.is(res.json.error, undefined, 'expect undefined');
+  response.data = td1;
+  const res = response.json;
+  t.is(res.status, 200, 'expecting OK response code');
+  t.is(res.message, 'OK', 'expecting OK');
+  t.deepEqual(res.data, [], 'expect deep equality to passed value');
+  t.is(res.error, undefined, 'expect undefined');
+  t.is(JSON.stringify(res), '{"status":200,"message":"OK","data":[]}', 'stringify equal to expected');
 });
 
 test('ServerResponse: adding one data to request', t => {
-  const res = new ServerResponse();
+  const response = new ServerResponse();
   const td1 = new TestData(0, 'test')
-  res.data = [td1];
-  t.is(res.json.status, 200, 'expecting OK response code');
-  t.is(res.json.message, 'OK', 'expecting OK');
-  t.deepEqual(res.json.data, [td1.toJson()], 'expect deep equality to passed value');
-  if(res.json.data == undefined) {
+  response.data = [td1];
+  const res = response.json;
+  t.is(res.status, 200, 'expecting OK response code');
+  t.is(res.message, 'OK', 'expecting OK');
+  t.deepEqual(res.data, [td1.toJson()], 'expect deep equality to passed value');
+  if(res.data == undefined) {
     t.fail('should not be undefined')
     return;
   }
-  t.is(res.json.data.length, 1, 'one element');
-  t.deepEqual(res.json.data[0], {id: 0, value: 'test'}, 'expect deep equality to this json literal');
-  t.is(res.json.error, undefined, 'expect undefined');
+  t.is(res.data.length, 1, 'one element');
+  t.deepEqual(res.data[0], {id: 0, value: 'test'}, 'expect deep equality to this json literal');
+  t.is(res.error, undefined, 'expect undefined');
+  t.is(JSON.stringify(res), '{"status":200,"message":"OK","data":[{"id":0,"value":"test"}]}', 'stringify equal to expected');
 });
 
 test('ServerResponse: adding null to request', t => {
-  const res = new ServerResponse();
-  res.data = null;
-  t.is(res.json.status, 204, 'expecting no content code');
-  t.is(res.json.message, 'NO_CONTENT', 'expecting NO_CONTENT');
-  t.is(res.json.data, undefined, 'expect undefined');
-  t.is(res.json.error, undefined, 'expect undefined');
+  const response = new ServerResponse();
+  response.data = null;
+  const res = response.json;
+  t.is(res.status, 204, 'expecting no content code');
+  t.is(res.message, 'NO_CONTENT', 'expecting NO_CONTENT');
+  t.is(res.data, undefined, 'expect undefined');
+  t.is(res.error, undefined, 'expect undefined');
+  t.is(JSON.stringify(res), '{"status":204,"message":"NO_CONTENT"}', 'stringify equal to expected');
 });
 
 test('ServerResponse: adding undefined to request', t => {
-  const res = new ServerResponse();
-  res.data = undefined;
-  t.is(res.json.status, 204, 'expecting no content code');
-  t.is(res.json.message, 'NO_CONTENT', 'expecting NO_CONTENT');
-  t.is(res.json.data, undefined, 'expect undefined');
-  t.is(res.json.error, undefined, 'expect undefined');
+  const response = new ServerResponse();
+  response.data = undefined;
+  const res = response.json;
+  t.is(res.status, 204, 'expecting no content code');
+  t.is(res.message, 'NO_CONTENT', 'expecting NO_CONTENT');
+  t.is(res.data, undefined, 'expect undefined');
+  t.is(res.error, undefined, 'expect undefined');
+  t.is(JSON.stringify(res), '{"status":204,"message":"NO_CONTENT"}', 'stringify equal to expected');
+});
+
+test('ServerResponse: sending 404 code', t => {
+  const response = new ServerResponse();
+  /* Use case: some logic goes here, try to fetch from DB */
+  /* But the SQL query returned no rows! */
+  response.data = [];
+  /* Lets override code to 404 and return */
+  response.status = 404;
+  const res = response.json;
+  t.is(res.status, 404, 'expecting not found code');
+  t.is(res.message, 'NOT_FOUND', 'expecting NOT_FOUND');
+  t.is(res.data, undefined, 'expect undefined');
+  t.is(res.error, undefined, 'expect undefined');
+  t.is(JSON.stringify(res), '{"status":404,"message":"NOT_FOUND"}', 'stringify equal to expected');
 });
