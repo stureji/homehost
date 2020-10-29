76f3efdee8d0f9ef3298bc261799ac6f0a928810
Erik Nord <tpi13eno@student.lu.se>
2020-10-29 16:47:45 +0100

    Can now fetch sectionless grocery

--- express/src/server/api/grocery.ts
+++ express/src/server/api/grocery.ts
@@ -7,19 +7,20 @@ import { QueryResult } from 'pg';
 import GrocerySignature from '../../database/schemes/GrocerySignature';
 const app = module.exports = express();
 
-const GET_ALL_GROCERY_QUERY = `SELECT
+const GET_ALL_QUERY = `SELECT
 grocery_id, grocery_name, grocery.section_id, section_name, sorting_order
 FROM grocery
 INNER JOIN section
 ON grocery.section_id = section.section_id`;
 
-const GET_GROCERY_BY_LETTER_QUERY = "SELECT * FROM grocery WHERE LOWER(grocery_name) LIKE $1 ORDER BY grocery_name";
+const GET_BY_LETTER_QUERY = "SELECT * FROM grocery WHERE LOWER(grocery_name) LIKE $1 ORDER BY grocery_name";
+const GET_SECTIONLESS = "SELECT * FROM grocery WHERE section_id IS NULL";
 
 app.get('/api/grocery/all', async (req: any, res: any) => {
   const response = new ServerResponse('GET', '/api/grocery/all');
 
   try {
-    const result: QueryResult<any> = await pool.query(GET_ALL_GROCERY_QUERY);
+    const result: QueryResult<any> = await pool.query(GET_ALL_QUERY);
     response.data = result.rows.map(r => {
       return new Grocery(r.grocery_id, r.grocery_name, new Section(r.section_id, r.section_name, r.sorting_order));
     });
@@ -42,11 +43,22 @@ app.post('/api/grocery/add', async(req: any, res: any) => {
 });
 
 app.get('/api/grocery/sectionless', async(req: any, res: any) => {
-  /**
-   * This API call will fetch all grocery that does not have a section assigned to it. Idea being
-   * that a user interface can be developed to allow an admin user to assign sections to groceries
-   * that currently do not have any.
-   */
+  const response = new ServerResponse('GET', '/api/grocery/sectionless');
+
+  try {
+    response.data = await pool.query(GET_SECTIONLESS).then((qres) => {
+      return qres.rows.map(r => {
+        return new GrocerySignature(r.grocery_id, r.grocery_name);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    if(error.code == 42501) {
+      response.status = 403;
+    }
+  }
+
+  res.status(response.status).json(response.json);
 });
 
 app.get('/api/grocery/:c', async(req: any, res: any) => {
@@ -54,11 +66,11 @@ app.get('/api/grocery/:c', async(req: any, res: any) => {
   const response = new ServerResponse('GET', '/api/grocery/' + c);
 
   try {
-    response.data = await pool.query(GET_GROCERY_BY_LETTER_QUERY, [c + '%']).then((qres) => {
+    response.data = await pool.query(GET_BY_LETTER_QUERY, [c + '%']).then((qres) => {
       return qres.rows.map(r => {
         return new GrocerySignature(r.grocery_id, r.grocery_name);
-      })
-    })
+      });
+    });
   } catch (error) {
     console.log(error);
     if(error.code == 42501) {
